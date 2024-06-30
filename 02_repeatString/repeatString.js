6fd7fb0b786edd46e05b9199eb4138572fbc6a71
Ignacio Dias Gundin <ignadiasg@gmail.com>
2024-06-30 16:05:44 -0300

    01,02,03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if(num == -1)
+        return "ERROR";
+    output = ""
+    for(i = 0; i < num; i++) {
+        output += str;
+    }
+    return output;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    reversed = "";
+    for(const letter of str) {
+        reversed = letter.toString() + reversed;
+    }
+    return reversed
 };
 
 // Do not edit below this line
